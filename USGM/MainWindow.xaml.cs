a28b7c995c7b47c8c2638600092d59aff7f6e5c2
Dmgvol <34919093+Dmgvol@users.noreply.github.com>
2022-03-04 01:14:41 +0200

    Initial upload

---
+++ USGM/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace USGM {
+    public partial class MainWindow : Window {
+
+        public MainWindow() {
+            InitializeComponent();
+            Config.GetInstance();
+            GetSaveFiles();
+            InitSaveSlots();
+        }
+
+        private void GetSaveFiles() {
+            if(!Directory.Exists(Config.GetInstance().SavesFolder)) {
+                MessageBox.Show("Can't find the \"Saves\" directory", "Directory not found");
+                return;
+            }
+            string[] files = Directory.GetFiles(Config.GetInstance().SavesFolder, "*.sav", SearchOption.AllDirectories);
+            saveListBox.Items.Clear();
+            foreach(string file in files) {
+                saveListBox.Items.Add(new ListBoxItem() { Content = Path.GetFileNameWithoutExtension(file) });
+            }
+        }
+
+        private void InitSaveSlots() {
+            for(int i = 0; i < Config.GetInstance().TotalSlots; i++) {
+                saveSlotSelector.Items.Add(Config.GetInstance().SaveFilePrefix.Replace("{n}", "" + (Config.GetInstance().SaveFileCountStart + i)));
+            }
+        }
+
+        private void LoadSelectedSave() {
+            // anything selected?
+            if(saveListBox.SelectedItem == null) {
+                MessageBox.Show("Please select a file to load!", "No file selected");
+                return;
+            }
+            // file to load
+            string fileToLoad = Config.GetInstance().SavesFolder + "\\" + ((ListBoxItem)saveListBox.SelectedItem).Content.ToString() + ".sav";
+            // still exists?
+            if(!File.Exists(fileToLoad)) {
+                MessageBox.Show("Save file can't be located. Has it been moved or deleted?", "File not found");
+                GetSaveFiles();
+                return;
+            }
+
+            // Try to copy
+            string pathToSave = Config.GetInstance().SaveFilePrefix.Replace("{n}", "" + (saveSlotSelector.SelectedIndex + Config.GetInstance().SaveFileCountStart)) + ".sav";
+            try {
+                File.Copy(fileToLoad, pathToSave, true);
+            } catch(Exception exception) {
+                MessageBox.Show(exception.Message, "Error");
+            }
+        }
+
+        private void refreshBtn_Click(object sender, RoutedEventArgs e) => GetSaveFiles();
+
+        private void loadSaveBtn_Click(object sender, RoutedEventArgs e) => LoadSelectedSave();
+    }
+}
---
+++ USGM/Utils/Config.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+namespace USGM {
+    public class Config {
+        private static Config Instance;
+        public static Config GetInstance() {
+            if(Instance == null) Instance = new Config();
+            return Instance;
+        }
+      
+        public string ConfigFileName { get; private set; } = "USGM.config";
+        public string SaveFilePrefix { get; private set; } = "SaveSlotIndex{n}";
+        public int SaveFileCountStart { get; private set; } = 1;
+        public int TotalSlots { get; private set; } = 3;
+        public string SavesFolder { get; private set; } = "saves";
+
+        private Config() {InitConfig();}
+
+        // load config
+        private void InitConfig() {
+            if(!File.Exists(ConfigFileName)) {
+                MessageBox.Show("Missing config file, creating default config - for SW3.\n(might not work with your game)", "Missing config file", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                // create new config
+                string CfgContent = nameof(SaveFilePrefix) + "=" + SaveFilePrefix + "\n" +
+                    nameof(SaveFileCountStart) + "=" + SaveFileCountStart + "\n" +
+                    nameof(TotalSlots) + "=" + TotalSlots + "\n";
+                File.WriteAllText(ConfigFileName, CfgContent);
+            }
+
+            var data = File.ReadAllLines(ConfigFileName);
+            for(int i = 0; i < data.Length; i++) {
+                var Par = GetParameter(data[i]);
+                if(Par.Item1 == nameof(SaveFilePrefix)) {
+                    SaveFilePrefix = Par.Item2;
+                } else if(Par.Item1 == nameof(SaveFileCountStart)) {
+                    if(IsNumeric(Par.Item2)) {
+                        SaveFileCountStart = int.Parse(Par.Item2);
+                    }
+                } else if(Par.Item1 == nameof(TotalSlots)) {
+                    if(IsNumeric(Par.Item2)) {
+                        TotalSlots = Clamp(int.Parse(Par.Item2), 1, 10);
+                    }
+                }
+            }
+        }
+
+        private Tuple<string, string> GetParameter(string data) {
+            var split = data.Split('=');
+            return split?.Length == 2 ? new Tuple<string, string>(split[0], split[1]) : null;
+        }
+
+        public static bool IsNumeric(string value) => float.TryParse(value, out _);
+
+        public static int Clamp(int value, int min, int max) {
+            if(value < min) return min;
+            if(value > max) return max;
+            return value;
+        }
+    }
+}
